41cdd05b2bddbc81a41013f00c4159428889aa0d
Ekhiotz <ekhiotz.garay@opendeusto.es>
2024-09-20 08:36:32 +0200

    hhhh

--- pruebagit/src/pruebagit/HolaMundo.java
+++ pruebagit/src/pruebagit/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println(" Hola Adios Mundo");
 		System.out.println("  Adios Mundo");
-
+		System.out.println("BUENAS");
 		
 	}
 
